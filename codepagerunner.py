08f7068dae238257a6b4121c02da0e51b7b82208
Destructible-Watermelon <halfling.wizard123@gmail.com>
2017-06-26 12:42:20 +0930

    made it match with sadflak.py

--- codepagerunner.py
+++ codepagerunner.py
@@ -1,8 +1,8 @@
 import argparse
 import sadflak
 class SpecialCodePageSadFlak(sadflak.SadflakInterpreter):
-    def __init__(self,program):
-        self.program = [i.replace("\n","").translate(str.maketrans("`~≤≥","≤≥`~")) for i in open(program).readlines()]
+    def __init__(self,arguments):
+        self.program = [i.replace("\n","").translate(str.maketrans("`~≤≥","≤≥`~")) for i in open(arguments.program).readlines()]
         self.expand()
         for i in range(len(self.program)):
             if self.program[i]:
@@ -11,15 +11,15 @@ class SpecialCodePageSadFlak(sadflak.SadflakInterpreter):
                 self.program[i] = None
         self.stacks = sadflak.TwinStacks()
 
-        import argparse
-        parser = argparse.ArgumentParser(description="Interpret the Sadflak language")
-        parser.add_argument(
-        "-A", "--ascii_output", action="store_true",
-        help="ascii output"
-        )
-        argv = parser.parse_args()
-        self.ascii_output = argv.ascii_output
+        self.ascii_output = arguments.ascii_output
 
 if __name__ == "__main__":
-    SpecialCodePageSadFlak(input()).run(input())
+    import argparse
+    parser = argparse.ArgumentParser(description="Interpret the Sadflak language with special codepage")
+    parser.add_argument("program", metavar="program", type=str, help="path to the program")
+    parser.add_argument("-A", "--ascii_output", action="store_true", help="ascii output")
+
+    import os, sys
+    the_input = '' if os.isatty(0) else sys.stdin.read()
+    SpecialCodePageSadFlak(parser.parse_args()).run(the_input)
 
